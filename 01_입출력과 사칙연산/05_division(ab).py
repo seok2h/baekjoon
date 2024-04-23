d1a6059740fa3664556c3757cb39b9a8a010ef12
seok2h <cream5432111@gmail.com>
2024-04-23 17:44:56 +0900

    Add 01_입출력과 사칙연산

---
+++ 01_입출력과 사칙연산/05_division(ab).py
@@ -0,0 +1,26 @@
+"""
+https://www.acmicpc.net/problem/1008
+제목 : A/B
+번호 : 1008
+
+문제
+두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+"""
+
+
+string = input()
+
+var = string.split(" ")
+
+p1 = int(var[0])
+p2 = int(var[1])
+
+answer = p1/p2
+
+print(answer)
