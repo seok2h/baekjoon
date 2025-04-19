e3191392d08515c4adea7449ca3f9ee20c2ad03b
me <me@me>
2025-04-19 10:50:07 +0000

    finish 16

--- 16_스택, 큐, 덱/09.py
+++ 16_스택, 큐, 덱/09.py
@@ -25,44 +25,63 @@ url: https://www.acmicpc.net/problem/28279
 출력을 요구하는 명령이 주어질 때마다 명령의 결과를 한 줄에 하나씩 출력한다.
 '''
 
+# import sys
+# from collections import deque
+
+# n = int(sys.stdin.readline())
+# queue = deque()
+
+# for _ in range(n):
+#     command = sys.stdin.readline().split()
+    
+#     if command[0] == '1':
+#         queue.appendleft(command[1])
+#     elif command[0] == '2':
+#         queue.append(command[1])
+#     elif command[0] == '3':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             print(queue.popleft())
+#     elif command[0] == '4':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             print(queue.pop())
+#     elif command[0] == '5':
+#         print(len(queue))
+#     elif command[0] == '6':
+#         if len(queue) == 0:
+#             print(1)
+#         else:
+#             print(0)
+#     elif command[0] == '7':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             print(queue[0])
+#     elif command[0] == '8':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             print(queue[-1])
+    
 import sys
 from collections import deque
 
 n = int(sys.stdin.readline())
-queue = deque()
+list_a = list(map(int, sys.stdin.readline().split()))
+list_b = list(map(int, sys.stdin.readline().split()))
 
-for _ in range(n):
-    command = sys.stdin.readline().split()
-    
-    if command[0] == '1':
-        queue.appendleft(command[1])
-    elif command[0] == '2':
-        queue.append(command[1])
-    elif command[0] == '3':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue.popleft())
-    elif command[0] == '4':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue.pop())
-    elif command[0] == '5':
-        print(len(queue))
-    elif command[0] == '6':
-        if len(queue) == 0:
-            print(1)
-        else:
-            print(0)
-    elif command[0] == '7':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue[0])
-    elif command[0] == '8':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue[-1])
-    
+m = int(sys.stdin.readline())
+list_c = list(map(int, sys.stdin.readline().split()))
+
+res = deque()
+
+for qs in range(n):
+    if list_a[qs] == 0:
+        res.appendleft(list_b[qs])
+
+for i in range(m):
+    res.append(list_c[i])
+    print(res.popleft(), end=' ')
