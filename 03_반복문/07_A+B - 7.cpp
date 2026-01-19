af8088848c2483b548692fffd413a84e7894c05f
Han Seungseok <cream5432111@gmail.com>
2026-01-19 17:23:25 +0900

    2026-01-19 study 2

---
+++ 03_반복문/07_A+B - 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int a, b;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a + b << "\n";
+    }
+    return 0;
+}
