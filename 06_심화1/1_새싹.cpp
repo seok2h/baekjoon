5d2c93fe1198d878bc8f6d75a6919b72da2af5d9
Han Seungseok <cream5432111@gmail.com>
2026-02-03 19:17:09 +0900

    2026-02-03 study 5

---
+++ 06_심화1/1_새싹.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s = "         ,r\'\"7\n";
+    s += "r`-_   ,'  ,/\n";
+    s += " \\. \". L_r'\n";
+    s += "   `~\\/\n";
+    s += "      |\n";
+    s += "      |\n";
+
+    cout << s;
+
+    return 0;
+}
