efcdd088b222d9072927af9cd78675979029173f
me <me@me>
2026-02-14 11:03:19 +0000

    2026-02-14 study 3

---
+++ 09_약수, 배수와 소수/01_배수와 약수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    while (true) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) return 0;
+
+        if ((a > b) && (a % b == 0)) {
+            cout << "multiple\n";
+        }
+        else if((a < b) && (b % a == 0)) {
+            cout << "factor\n";
+        }
+        else {
+            cout << "neither\n";
+        }
+        
+    }
+    return 0;
+}
